c46e29c584f528a5f82ad95b5689622e901d85b0
SamuelPlaton <platonsam02@gmail.com>
2023-05-12 13:39:32 +0200

    feat: implement erc721 contract

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,9 +1,11 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 const SamuelitoToken = artifacts.require("SamuelitoToken")
+const SamuelitoItem = artifacts.require("SamuelitoItem")
 
 module.exports = function(deployer) {
   //deployer.deploy(ConvertLib);
   //deployer.link(ConvertLib, SamuelitoToken);
-  deployer.deploy(SamuelitoToken);
+  //deployer.deploy(SamuelitoToken);
+  deployer.deploy(SamuelitoItem);
 };
